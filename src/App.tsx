6cb6d1fdc00199bfe06b18e11e748016f1992af0
dn.kalabin <kalabin.danil28@mail.ru>
2024-08-05 19:55:59 +0300

    new init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
 
+import WebApp from '@twa-dev/sdk'
+import { useState } from 'react'
+
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      {/* Here we add our button with alert callback */}
+      <div className="card">
+        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+          Show Alert
+        </button>
+      </div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import './index.css'
+
+import App from './App.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import WebApp from '@twa-dev/sdk'
+
+WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
